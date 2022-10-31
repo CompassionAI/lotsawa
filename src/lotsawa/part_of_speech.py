7ba9cc10085d9d652188cddf41d138ed5a097863
eisene <eeisenst@gmail.com>
2022-10-31 13:46:15 -0400

    Split out interactive mode in PoS tagger
    Also add Hydra colors

--- src/lotsawa/part_of_speech.py
+++ src/lotsawa/part_of_speech.py
@@ -2,7 +2,20 @@ import sys
 import logging
 import hydra
 
+from hydra.utils import instantiate
 from cai_manas.part_of_speech.pos_tagger import PartOfSpeechTagger
+
+
+def interactive(tagger, _mode_cfg):
+    print("Interactive Tibetan part-of-speech tagging...")
+    while True:
+        print("===")
+        bo_text = input("Tibetan (or type exit): ")
+        if bo_text == "exit":
+            break
+        res = tagger.tag(bo_text)
+        for word, tag in zip(res["words"], res["tags"]):
+            print(word, tag)
 
 
 @hydra.main(version_base="1.2", config_path="token_classification_config", config_name="part_of_speech")
@@ -16,15 +29,7 @@ def main(cfg):
 
     tagger = PartOfSpeechTagger(cfg.model.model_ckpt)
 
-    print("Interactive Tibetan part-of-speech tagging...")
-    while True:
-        print("===")
-        bo_text = input("Tibetan (or type exit): ")
-        if bo_text == "exit":
-            break
-        res = tagger.tag(bo_text)
-        for word, tag in zip(res["words"], res["tags"]):
-            print(word, tag)
+    instantiate(cfg.mode.process_func, tagger, cfg.mode)
 
 
 if __name__ == "__main__":
